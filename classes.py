14d98d1d7d7dff5e9039c5a1c721ada675262802
cuajiniquil <157638304+cuajiniquil@users.noreply.github.com>
2026-03-22 15:39:39 +0100

    tournoi 1

--- classes.py
+++ classes.py
@@ -2,14 +2,10 @@ import random as rd
 import math
 import copy as cp
 import json
-import matplotlib
-
 
 with open("nombres.json","r") as nom:
     listNmbr = json.load(nom)
 
-   
-#mira esto de aqui
 # On definit le planète par son nom (composé de 2 noms), et ses coordonées
 class Planete:
     def __init__(self,NomP: str, x: int, y: int): #__init__ sert à initialiser la classe pour pouvoir assigner les valeurs
@@ -23,7 +19,7 @@ class Planete:
         # ! que hace repr? Es como ToString en Java
         return f"[{self.NomP} | Coordonnées: ({self.x}, {self.y})]"
         
-class Galaxia: #GENERA LOS PLANETAS UNICAMENTE 
+class Galaxie: #GENERA LOS PLANETAS UNICAMENTE 
 
     def __init__(self):
         self.listPlnt: list[Planete] = []
@@ -71,64 +67,70 @@ class Galaxia: #GENERA LOS PLANETAS UNICAMENTE
     
     
 #MEZCLA LA GALAXIA PARA OBTENER M ITINERARIOS DIFERENTES
-class Itinerarios():
-    def __init__(self, Galx : Galaxia):
+class Itineraires():
+
+    def __init__(self, Galx : Galaxie):
         self.Galx = Galx
         self.distTot = 0
-
         self.itinerarios = []
 
-   #def mutation(self, p: int): # aqui los self.Galx.listPlnt[i] me dan errores del estilo No overloads for "__setitem__" match the provided arguments pero aun asi fucniona en main
-    #    for i in self.Galx.listPlnt:
-     #       if(rd.randint(0,100)<= p*100):#porque p*100 y cuando defines p, no veo que le hallas dado algun valor
-      #          j = self.Galx.listPlnt[rd.randint(0, len(self.Galx.listPlnt)-1)]
-       #         self.Galx.listPlnt[i], self.Galx.listPlnt[j] = self.Galx.listPlnt[j], self.Galx.listPlnt[i] # ! esto es para mezclar la lista de planetas, pero no se si es la mejor forma de hacerlo
-    
-    #MODIFS - PARA QUE SEA COMO EL PROBLEMA: EL PRIMER PLANETA ES EL MISMO PARA TODOS LOS ITINERARIOS, ASI QUE NO SE MEZCLA EL PRIMER PLANETA
-    #generation aleatoire de m itineraires
+   #generation aleatoire de m itineraires
     def genererRd(self, m: int):
+        self.itinerarios = [] # evite duplicats avec boucle de simulations
+        plnt_init = self.Galx.listPlnt[0]
         for i in range(m):
-            shuflPlnt = self.Galx.listPlnt.copy() 
-            rd.shuffle(shuflPlnt)
-            self.itinerarios.append(shuflPlnt)
-
-    #generation de m itineraires a partir de itinerarios existentes
-    def genererHerit(self, m: int, p: int):
-        for i in range(m):
-            shuflPlnt = self.itinerarios[i]
-            #self.mutation(p)
-            self.itinerarios.append(shuflPlnt)
+            chemin = self.Galx.listPlnt[1:]
+            rd.shuffle(chemin)
+            self.itinerarios.append([plnt_init] + chemin)
 
     def Dist(self, chemin):
-        if(len(chemin)>0):#el primer index es 0 asi que  decimos que calcule las distancias solo cuandi haya mas de 1 planeta
-            for i in range(len(chemin)-1):# va parcorrir todo la lista y va coger el x y el y del primer planeta y del siguiente
+        self.distTot = 0 # eviter accumulation..
+        if(len(chemin)>0):#el primer index es 0 asi que  decimos que calcule las distancias solo cuando haya mas de 1 planeta
+            for i in range(len(chemin)):# va parcorrir toute la liste et prend le x et y du premier planete et du suivant
                 p1 = chemin[i]#guarda el primero
-                p2 = chemin[i + 1]
+                p2 = chemin[(i + 1) % len(chemin)] #permet de creer un circuit fermé 
                 dist = math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) # formula de la distancia entre dos puntos en un plano cartesiano
                 self.distTot += dist
             return self.distTot
+
+    def mutation(self, p: float):
+        for chemin in self.itinerarios:
+            if rd.randint(1, 100) <= p * 100: # si el numero aleatorio es menor que la probabilidad de mutacion entonces se muta el itinerario
+                #indices commencent a 1 pour eviter de melanger le premier planete
+                i = rd.randint(1, len(chemin) - 1)
+                j = rd.randint(1, len(chemin) - 1)
+                chemin[i], chemin[j] = chemin[j], chemin[i] # intercambia los planetas en esos idx
+
+    def croisement(self, parent1, parent2): 
+        pass
+class Tournoi:
+    def __init__(self):
+        self.fitness = 0
+        self.chemin = []
+        self.FitnessList = []
+        self.distance = 0
+
     
-    def fitness(self, it):  
-        fitness_list = []
-        for i in range(len(it)):
-            d = self.Dist(it[i])
+    #Calcula la fitness
+    def fitness(self, itin : Itineraires):
+        for chemin in itin.itinerarios:
+            d = itin.Dist(chemin)
             if d != 0:
-                fitness_list.append(1 / d)
+                self.fitness = 1 / d
             else:
-                fitness_list.append(float('inf'))  # max au cas ou distance nulle (1 planete)
-        return fitness_list
-        
-    def nouvellegen(self, m: int, p: int):
-        prevgen = cp.copy(self)
-        #modificar para que esten tambien en order creciente
-        #creo que no es con prevgen, despues testeo
-        # m se puede remplazar por el tamano de la poblacion tambien
-        h = 1
-        n = 2
-        while(h > 0):
-            h = round(m/n**2)
-            prevgen.genererHerit(h, p)
-            n += 1
-        #que el resto de los itinerarios sean aleatorios (como los "faibles" del torneo de akshay)
-        for i in range(m - h): #no es h, si no un contador de cuanos itinerarios se han generado con la herencia, porque h es el numero de itinerarios generados en la ultima iteracion del while, pero antes se han generado otros h itinerarios, entonces el numero de itinerarios faltan para llegar a m 
-            prevgen.genererRd(1) #porque 1?
+                self.fitness = float('inf')
+            self.FitnessList.append((chemin,self.fitness, d))
+        return self.FitnessList
+    
+    def tournoi(self, rounds: int):
+        scores = [0] * len(self.FitnessList) # inicializa los puntajes de cada itinerario a 0
+        for i in range(rounds):  
+            idx = rd.sample(range(len(self.FitnessList)), 5)
+    # escoge al mejor
+            best = idx[0]
+            for i in idx:
+                if self.FitnessList[i][1] > self.FitnessList[best][1]:
+                    best = i
+    # suma un punto al vencedor
+            scores[best] += 1
+    
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def start_simulation():
     nbi = boite_reglages.get_nbi() 
     #nbg = boite_reglages.get_nbg() #remettre apres avoir fait la partie generations
 
-    galaxie = classes.Galaxia() # iniciar la galaxia
+    galaxie = classes.Galaxie() # iniciar la galaxia
     galaxie.SamsungGalaxy(nbp) # generar los planetas
     sprites = []
     for planet in galaxie.listPlnt:
@@ -30,48 +30,12 @@ def start_simulation():
     etat_sim_graph["sprites"] = sprites
     
 
-    ittest = classes.Itinerarios(galaxie) # test de itinerarios
+    ittest = classes.Itineraires(galaxie) # test de itinerarios
     ittest.genererRd(nbi) # generer les itinéraires
 
     for i in range(nbi):
         graphics.GalaxyPath(canvas, ittest.itinerarios[i])
- # ! puse fitness en clases para que se pueda usar en main, pero si causa problemas lo devolvemos a main
-    fitness_list = ittest.fitness(ittest.itinerarios)
-    #crea lista con tantos 0 como caminos en la lista fitness
-    scores = [0]* len(fitness_list) 
-
-#L46-L67: TORNEO, NO SIRVE POR AHORA  
-    for i in range(50):  
-    # selecciona a 5 itinerarios
-        indices = rd.sample(range(len(fitness_list)), 5)
-    # escoge al mejor
-        best = indices[0]
-        for i in indices:
-            if fitness_list[i][1] > fitness_list[best][1]:
-                best = i
-    # suma un punto al vencedor
-            scores[best] += 1
-
-
-    resultats = []
-
-    for i in range(len(fitness_list)):
-        chemin, fitness, distance = fitness_list[i]
-        score = scores[i]
-        resultats.append((chemin, fitness, distance, score))
-
-# stockea los 4 mejores
-    meilleurs = resultats[:4]
-
-#prints para verificar que todo funciona
-    print("MEILLEURS CHEMINS :")
-    #for chemin, fitness, distance, score in meilleurs:
-    for i in range(len(fitness_list)):
-        print("Score :", scores[i])
-        print("Distance :", ittest.Dist(ittest.itinerarios[i]))
-        print("Fitness :", fitness_list[i])
-        print("--------------")
-
+        print("distance itinéraire", i, ":", ittest.Dist(ittest.itinerarios[i]))
 boite_reglages.start_cmd(start_simulation)
 
 root.mainloop() # hace correr el programa no se debe de poner nada despues de esta linea segun stack overflow porque si no el programa lo ignorara
